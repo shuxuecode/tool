d539816ad22240ab605fe7f7e263a227e0dfd4ce
zhaoshuxue <zhaoshuxue@163.com>
2024-07-03 01:54:55 +0800

    :hammer: :card_file_box: :alien:

--- noteByGit/js/main.js
+++ noteByGit/js/main.js
@@ -71,21 +71,28 @@ var $repo = undefined
 var $filePath = undefined
 
 
-var pullContent = function () {
-
+var getConfig = function () {
     $token = document.getElementById('token').value
     $owner = document.getElementById('owner').value
     $repo = document.getElementById('repo').value
     $filePath = document.getElementById('filePath').value
 
     if($token == '' || $owner == '' || $repo == '' || $filePath == '') {
-        showTitle("配置为空")
         toastr.error("配置为空")
-        return
+        return false
     }
 
     octo = new Octokat({ token: $token })
     repo = octo.repos($owner, $repo)
+
+    return true
+}
+
+var pullContent = function () {
+
+    if (getConfig() === false) {
+        return
+    }
 
     // repo.contents($filePath).read() // Use `.read` to get the raw file.
     //     .then((contents) => {        // `.fetch` is used for getting JSON
@@ -97,16 +104,18 @@ var pullContent = function () {
         .then((info) => {
             // console.log(info.sha, info.content)
             sha = info.sha
-            
+            showTitle("sha=" + sha)
             var contents = base64Decode(info.content)
             document.getElementById("content").value = contents;
+
+            toastr.info("pull成功")
         });
 }
 
 var pushContent = function () {
 
     if(repo == undefined || $filePath == '') {
-        showTitle("repo未配置")
+        toastr.error("repo未配置")
         return
     }
 
@@ -122,8 +131,62 @@ var pushContent = function () {
     repo.contents($filePath).add(config)
         .then((info) => {
             console.log('File Updated. new sha is ', info.commit.sha)
-            sha = info.commit.sha
+            // sha = info.commit.sha
+            // showTitle("sha=" + sha)
+
+            toastr.info("推送成功")
+
+            // 重新拉取
+            pullContent()
         })
+}
+
+
+
+
+var getFileList = function () {
+   
+    $token = document.getElementById('token').value
+    $owner = document.getElementById('owner').value
+    $repo = document.getElementById('repo').value
+    $filePath = document.getElementById('filePath').value
+
+    if($token == '' || $owner == '' || $repo == '') {
+        toastr.error("配置为空")
+        return false
+    }
+
+    octo = new Octokat({ token: $token })
+    repo = octo.repos($owner, $repo)
+
+    // repo.contents('').fetch().then(contents => {
+    repo.contents($filePath).fetch().then(contents => {
+        // 'contents' is an array of files and directories in the repository's root
+        //   console.log(contents)
+        //   console.log(contents.items)
+
+        var html = ''
+
+        contents.items.forEach(file => {
+            console.log(`path: ${file.path}, type: ${file.type}`);
+
+            if (file.type == 'dir') {
+                html += '<li onclick="setNewFilePath(this)" class="dirCls">' + file.path + '</li>'
+            } else {
+                html += '<li onclick="setNewFilePath(this)">' + file.path + '</li>'
+            }
+            
+        });
+
+        $('#fileList').html(html)
+    });
+}
+
+
+function setNewFilePath(e){
+    console.info(e)
+    console.info($(e))
+    console.info($(e).text())
 }
 
 
@@ -146,6 +209,9 @@ var init = function () {
     document.getElementById('pullBtn').onclick = pullContent;
     document.getElementById('pushBtn').onclick = pushContent;
 
+    $('#getFileBtn').on('click', function(){
+        getFileList()
+    })
 
     // 初始化内容
     getToken();
