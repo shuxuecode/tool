2791686a3760adab4faf81c471e0637081e2e34b
shuxuecode <zhaoshuxue@163.com>
2024-11-03 22:22:48 +0800

    :mute: :loud_sound: :passport_control:

---
+++ lovenote/js/api.js
@@ -0,0 +1,170 @@
+
+var createNewBranch = async function () {
+
+    $branch = $('#branch').val().trim()
+
+    if ($branch == '') {
+        toastr.error("新分支为空")
+        return;
+    }
+
+    const defaultBranch = await getDefaultBranch()
+
+
+    repo.branches(defaultBranch).fetch()
+        .then((branchInfo) => {
+            baseBranchSHA = branchInfo.commit.sha; // 获取SHA
+            // 继续用这个SHA创建新分支
+
+
+            var newBranchName = $branch
+
+            // 创建新分支的引用
+            repo.git.refs.create({
+                ref: `refs/heads/${newBranchName}`,
+                sha: baseBranchSHA
+            }).then(() => {
+                console.log(`分支 ${newBranchName} 创建成功`);
+
+                toastr.success(`分支 ${newBranchName} 创建成功`)
+
+            }).catch((error) => {
+                console.error('创建新分支时出现错误: ', error);
+            });
+
+
+
+        })
+        .catch((error) => {
+            console.error('无法获取基准分支的SHA: ', error);
+        });
+}
+
+
+var deleteFileFun = function () {
+
+    $filePath = document.getElementById('filePath').value
+
+    if (repo == undefined || $filePath == '') {
+        toastr.error("repo未配置")
+        return
+    }
+
+    var config = {
+        message: 'Deleting file',
+        sha: sha
+    }
+
+    $branch = $('#branch').val().trim()
+    if ($branch != '') {
+        config.branch = $branch
+    }
+
+    repo.contents($filePath).remove(config)
+        .then((result) => {
+            console.log('File Deleted. result is ', result)
+            toastr.info("删除成功")
+        })
+        .catch((error) => {
+            console.error('An error occurred while deleting the file', error);
+            toastr.error("删除失败")
+        });
+}
+
+
+// GitHub API 并不直接支持重命名文件。要重命名文件，通常的做法是创建一个新文件并删除旧文件
+
+var renameFile = function (newFileName) {
+// todo 
+    if (newFileName == $filePath) {
+        toastr.error("文件名一模一样，请修改")
+        return
+    }
+
+    $branch = $('#branch').val().trim()
+
+    // 第一阶段：获取旧文件的内容和 SHA
+    repo.contents($filePath)
+        .fetch({ref: $branch})
+        .then((info) => {
+
+            var config = {
+                message: `Rename file ${$filePath} to ${newFileName}`, // commit信息
+                content: info.content
+            }
+            if ($branch != '') {
+                config.branch = $branch
+            }
+            
+            // 第二阶段：创建新文件
+            repo.contents(newFileName).add(config)
+                .then((result) => {
+                    console.log('File add success. result is ', result)
+        
+                    // toastr.info("添加成功")
+                    // 重新拉取
+                    // pullContent()
+
+
+                    var config = {
+                        message: 'Deleting file',
+                        sha: info.sha
+                    }
+                
+                    if ($branch != '') {
+                        config.branch = $branch
+                    }
+                
+                    // 第三阶段：删除旧文件
+                    repo.contents($filePath).remove(config)
+                        .then((res) => {
+                            console.log('File Deleted. result is ', res)
+                            toastr.info("重命名成功")
+                            // 更新文件名
+                            document.getElementById('filePath').value = newFileName
+
+                        })
+                        .catch((err) => {
+                            console.error('An error occurred while deleting the file', err);
+                            toastr.error("删除失败")
+                        });
+
+                }).catch(error => {
+                    console.error("Failed to create file: ", error)
+                })
+
+        });
+}
+
+
+// 创建issue
+var createIssuesFun = function () {
+
+    if (repo == undefined) {
+        toastr.error("repo未配置")
+        return
+    }
+
+    var issues = document.getElementById("issueContent").value;
+    if (issues == '') {
+        toastr.error("issue内容为空")
+        return
+    }
+
+    var title = document.getElementById("issueTitle").value;
+    title = title == '' ? "Issue 标题" : title;
+
+    repo.issues.create({
+        title: title,
+        body: issues
+      })
+      .then(response => {
+        console.log('Issue 创建成功:', response);
+        toastr.info("创建成功")
+      })
+      .catch(error => {
+        console.error('创建 Issue 时出错:', error);
+        toastr.error("创建失败")
+      });
+
+}
---
+++ lovenote/js/main.js
@@ -0,0 +1,491 @@
+
+
+
+
+
+
+
+function setToken() {
+    localStorage.setItem('my_github_token', $("#token").val());
+    toastr.success("设置token成功")
+}
+
+function getToken() {
+    var token = localStorage.getItem('my_github_token');
+    $('#token').val(token)
+}
+
+function removeAll() {
+    localStorage.removeItem('my_github_token');
+    localStorage.removeItem('my_github_owner');
+    localStorage.removeItem('my_github_repo');
+    localStorage.removeItem('my_github_filepath');
+}
+
+// 
+function setOwner() {
+    localStorage.setItem('my_github_owner', document.getElementById('owner').value);
+    toastr.success("设置owner成功")
+}
+
+function getOwner() {
+    document.getElementById('owner').value = localStorage.getItem('my_github_owner');
+}
+
+// 
+function setRepo() {
+    localStorage.setItem('my_github_repo', document.getElementById('repo').value);
+
+    $token = document.getElementById('token').value
+    $owner = document.getElementById('owner').value
+    $repo = document.getElementById('repo').value
+    
+    if ($token == '' || $owner == '' || $repo == '') {
+        toastr.error("配置为空")
+        return false
+    }
+
+    octo = new Octokat({ token: $token })
+    repo = octo.repos($owner, $repo)
+
+    toastr.success("设置repo成功")
+}
+
+function getRepo() {
+    document.getElementById('repo').value = localStorage.getItem('my_github_repo');
+}
+
+function setBranch() {
+    localStorage.setItem('my_github_branch', document.getElementById('branch').value);
+    toastr.success("设置分支成功")
+}
+
+function getBranch() {
+    document.getElementById('branch').value = localStorage.getItem('my_github_branch');
+}
+
+// 
+function setFilePath() {
+    localStorage.setItem('my_github_filepath', document.getElementById('filePath').value);
+    toastr.success("设置filePath成功")
+}
+
+function getFilePath() {
+    document.getElementById('filePath').value = localStorage.getItem('my_github_filepath');
+}
+
+
+
+
+var octo = undefined
+var repo = undefined
+var sha = undefined;
+
+var $token = undefined
+var $owner = undefined
+var $repo = undefined
+var $branch = ''
+var $filePath = undefined
+
+
+var initRepo = function () {
+    $token = document.getElementById('token').value
+    $owner = document.getElementById('owner').value
+    $repo = document.getElementById('repo').value
+    $filePath = document.getElementById('filePath').value
+
+    if ($token == '' || $owner == '' || $repo == '') {
+        toastr.error("配置为空")
+        return false
+    }
+
+    octo = new Octokat({ token: $token })
+    repo = octo.repos($owner, $repo)
+}
+
+var pullContent = function () {
+
+    if (repo === undefined) {
+        toastr.error("repo未设置")
+        return
+    }
+
+    // repo.contents($filePath).read() // Use `.read` to get the raw file.
+    //     .then((contents) => {        // `.fetch` is used for getting JSON
+    //         console.log(contents)
+    //         document.getElementById("content").value = contents;
+    //     });
+
+    $branch = $('#branch').val().trim()
+
+    repo.contents($filePath)
+        // .read({ref: $branch})
+        .fetch({ref: $branch})
+        .then((info) => {
+            console.log(info)
+            // console.log(info.sha, info.content)
+            sha = info.sha
+            showTitle("sha=" + sha)
+            var contents = base64Decode(info.content)
+            // var contents = info
+            document.getElementById("content").value = contents;
+
+            // 预览
+            showPreview(contents);
+
+            toastr.info("pull成功")
+        });
+}
+
+var pushContent = function () {
+    
+    $filePath = document.getElementById('filePath').value
+
+    if (repo == undefined || $filePath == '') {
+        toastr.error("repo未配置")
+        return
+    }
+
+    var text = document.getElementById("content").value
+
+    var config = {
+        message: 'Updating file',
+        content: base64Encode(text),
+        sha: sha, // the blob SHA
+        // branch: ''
+    }
+
+    $branch = $('#branch').val().trim()
+    if ($branch != '') {
+        config.branch = $branch
+    }
+
+    repo.contents($filePath).add(config)
+        .then((info) => {
+            console.log('File Updated. new sha is ', info.commit.sha)
+            // sha = info.commit.sha
+            // showTitle("sha=" + sha)
+
+            toastr.info("推送成功")
+            // 重新拉取
+            pullContent()
+        })
+}
+
+
+var addFile = function () {
+    if (repo == undefined) {
+        toastr.error("repo未配置")
+        return
+    }
+
+    $filePath = document.getElementById('filePath').value
+    if ($filePath == '') {
+        toastr.error("文件路径未配置")
+        return
+    }
+
+    var text = document.getElementById("content").value
+
+    var config = {
+        message: 'add file', // commit信息
+        content: base64Encode(text)
+    }
+
+    $branch = $('#branch').val().trim()
+    if ($branch != '') {
+        config.branch = $branch
+    }
+
+    repo.contents($filePath).add(config)
+        .then((result) => {
+            console.log('File add success. result is ', result)
+
+            toastr.info("添加成功")
+            // 重新拉取
+            // pullContent()
+        }).catch(error => {
+            console.error("Failed to create file: ", error)
+        })
+}
+
+
+
+
+
+var getFileList = function () {
+
+    $token = document.getElementById('token').value
+    $owner = document.getElementById('owner').value
+    $repo = document.getElementById('repo').value
+    $branch = $('#branch').val().trim()
+    $filePath = document.getElementById('filePath').value
+
+    if ($token == '' || $owner == '' || $repo == '') {
+        toastr.error("配置为空")
+        return false
+    }
+
+    octo = new Octokat({ token: $token })
+    repo = octo.repos($owner, $repo)
+
+    repo.contents('')
+        // .read({ref: ''})
+        .fetch({ref: $branch})
+        .then(contents => {
+    // repo.contents($filePath).fetch().then(contents => {
+        // 'contents' is an array of files and directories in the repository's root
+        //   console.log(contents)
+        //   console.log(contents.items)
+
+        var html = ''
+
+        contents.items.forEach(file => {
+            console.log(`path: ${file.path}, type: ${file.type}`);
+
+            if (file.type == 'dir') {
+                html += '<div class="_dir">'
+                html += '<button onclick="expandFile(this)" style="color: blue;">展开</button>'
+                html += '<span class="dirCls"> ' + file.path + '</span> '
+                html += '</div> '
+            } else {
+                // html += '<div onclick="setNewFilePath(this)">' + file.path + '</div>'
+                html += '<div class="_dir">'
+                html += '<button onclick="openFile(this)">打开</button>'
+                html += '<span class="dirCls2"> ' + file.path + '</span> '
+                html += '</div> '
+            }
+
+        });
+
+        $('#fileList').html(html)
+    });
+}
+
+function expandFile(e) {
+    // console.info($(e))
+    // console.info($(e).next())
+    // 获取文件路径
+    // console.info($(e).next().text())
+    var filePath = $(e).next().text().trim()
+
+    repo.contents(filePath)
+        // .read({ref: ''})    
+        .fetch({ref: $branch}) 
+        .then(contents => {
+
+        var html = ''
+
+        contents.items.forEach(file => {
+            console.log(`path: ${file.path}, type: ${file.type}`);
+
+            if (file.type == 'dir') {
+                html += '<div class="_dir">'
+                html += '<button onclick="expandFile(this)" style="color: blue;">展开</button>'
+                html += '<span class="dirCls"> ' + file.path + '</span> '
+                html += '</div> '
+            } else {
+                // html += '<div onclick="setNewFilePath(this)">' + file.path + '</div>'
+                html += '<div class="_dir">'
+                html += '<button onclick="openFile(this)">打开</button>'
+                html += '<span class="dirCls2"> ' + file.path + '</span> '
+                html += '</div> '
+            }
+
+        });
+
+        // $(e).parent().after(html)
+        $(e).parent().append(html)
+    });
+    
+    // 禁用按钮
+    $(e).prop('disabled', true);
+    // 
+    $(e).css({'color':'lightgray'});
+}
+
+
+function openFile(e) {
+
+    var filePath = $(e).next().text().trim()
+
+    // 更新路径
+    $("#filePath").val(filePath)
+
+    repo.contents(filePath).fetch({ref: $branch}) 
+        .then((info) => {
+            // console.log(info.sha, info.content)
+            sha = info.sha
+            showTitle("sha=" + sha)
+            var contents = base64Decode(info.content)
+            document.getElementById("content").value = contents;
+
+            // 预览
+            showPreview(contents);
+
+            toastr.info("pull成功")
+        });
+
+}
+
+
+// 使用 .branches 方法来获取所有分支的信息
+async function getBranchList() {
+
+    const defaultBranch = await getDefaultBranch()
+
+    // console.error(defaultBranch)
+
+    repo.branches.fetch()
+        .then(branches => {
+            
+            var html = ''
+
+            console.warn('branches', branches)
+            // `branches` 将是一个包含仓库所有分支信息的数组
+            branches.items.forEach(branch => {
+                // console.log(branch.name); // 打印分支名称
+
+                html += '<div class="">'
+                html += '<button onclick="chooseBranch(this)">使用该分支</button>'
+                if(defaultBranch === branch.name) {
+                    html += '<span class="branCls"> ' + branch.name + '</span> '
+                } else {
+                    html += '<span class=""> ' + branch.name + '</span> '
+                }
+                
+                html += '</div> '
+            });
+
+            $('#branchList').html(html)
+            // 
+        })
+        .catch(error => {
+            console.error('Error fetching branches: ', error);
+        });
+    
+}
+
+
+async function getDefaultBranch() {
+    return new Promise((resolve, reject) => {
+        // 这里进行异步操作
+        // 获取仓库信息
+        repo.fetch()
+            .then(repoInfo => {
+                console.log('默认分支=', repoInfo.defaultBranch); // 打印默认分支的名称
+                resolve(repoInfo.defaultBranch);
+            })
+            .catch(error => {
+                console.error('Error fetching repository information: ', error);
+            });
+      });
+}
+
+
+function chooseBranch(e) {
+
+    var branch = $(e).next().text().trim()
+
+    // 更新分支
+    $("#branch").val(branch)
+}
+
+
+function renameFilePrompt() {
+    $filePath = document.getElementById('filePath').value
+    var newFileName = prompt("请输入新的文件名", $filePath)
+    if (newFileName != null) {
+        renameFile(newFileName);
+    }
+}
+
+
+function deleteFile() {
+    var confirm = window.confirm("确认删除文件?")
+    if (confirm) {
+        deleteFileFun();
+    }
+}
+
+
+var check = function () {
+
+    if ($('#token').val().trim() === '') {
+        $('#h2id').html("token未设置")
+        $('#h2id').css({ "color": "red" })
+        return;
+    }
+
+    if ($('#owner').val().trim() === '') {
+        $('#h2id').html("owner未设置")
+        $('#h2id').css({ "color": "red" })
+        return;
+    }
+
+    if ($('#repo').val().trim() === '') {
+        $('#h2id').html("repo未设置")
+        $('#h2id').css({ "color": "red" })
+        return;
+    }
+
+}
+
+
+var init = function () {
+
+    // 绑定事件
+    document.getElementById('updateToken').onclick = setToken;
+    document.getElementById('updateOwner').onclick = setOwner;
+    document.getElementById('updateRepo').onclick = setRepo;
+    document.getElementById('updateBranch').onclick = setBranch;
+    document.getElementById('updateFilePath').onclick = setFilePath;
+
+    document.getElementById('pullBtn').onclick = pullContent;
+    document.getElementById('pushBtn').onclick = pushContent;
+    document.getElementById('addFileBtn').onclick = addFile;
+
+    document.getElementById('createIssuesBtn').onclick = createIssuesFun;
+    
+
+    $('#getFileBtn').on('click', function () {
+        getFileList()
+    })
+
+    $('#getBranchBtn').on('click', function () {
+        getBranchList()
+    })
+    
+    $('#switchPreview').on('click', function () {
+
+        const flag = $("#preview").css("display")
+        if (flag == "none") {
+            $("#contentParent").width("50%")
+            $("#preview").show()
+        } else {
+            $("#preview").hide()
+            $("#contentParent").width("90%")
+        }
+    })
+
+    // 初始化内容
+    getToken();
+    getOwner();
+    getRepo();
+    getBranch();
+    getFilePath();
+
+    // 前置检查
+    check();
+
+    // 加载配置
+    initRepo();
+
+    if (repo != undefined) {
+        $('#h2id').html("repo加载成功")
+        $('#h2id').css({ "color": "green" })
+    }
+}
+
+
+init()
