e80a3196e6fd79fdfd1738812f28622f6524f4f6
shuxuecode <zhaoshuxue@163.com>
2024-11-29 22:58:41 +0800

    :pushpin: :egg: :busts_in_silhouette:

--- todolist/js/main.js
+++ todolist/js/main.js
@@ -79,20 +79,16 @@ function getFilePath() {
 
 var octo = undefined
 var repo = undefined
-var sha = undefined;
 
 var $token = undefined
 var $owner = undefined
 var $repo = undefined
-var $branch = ''
-var $filePath = undefined
 
 
 var initRepo = function () {
     $token = document.getElementById('token').value
     $owner = document.getElementById('owner').value
     $repo = document.getElementById('repo').value
-    $filePath = document.getElementById('filePath').value
 
     if ($token == '' || $owner == '' || $repo == '') {
         toastr.error("配置为空")
@@ -102,76 +98,6 @@ var initRepo = function () {
     octo = new Octokat({ token: $token })
     repo = octo.repos($owner, $repo)
 }
-
-var pullContent = function () {
-
-    if (repo === undefined) {
-        toastr.error("repo未设置")
-        return
-    }
-
-    $branch = ''; // $('#branch').val().trim()
-
-    repo.contents($filePath)
-        // .read({ref: $branch})
-        .fetch({ref: $branch})
-        .then((info) => {
-            console.log('info= ', info)
-            // console.log(info.sha, info.content)
-            sha = info.sha
-            showTitle("sha=" + sha)
-            var contents = base64Decode(info.content)
-            
-            // document.getElementById("content").value = contents;
-
-            $('#content').trumbowyg('html', contents);
-
-            toastr.info("pull成功")
-        });
-}
-
-var pushContent = function () {
-    
-    $filePath = document.getElementById('filePath').value
-
-    if (repo == undefined || $filePath == '') {
-        toastr.error("repo未配置")
-        return
-    }
-
-    // var text = document.getElementById("content").value
-    var text = $('#content').trumbowyg('html');
-
-    var config = {
-        message: 'Updating file',
-        content: base64Encode(text),
-        sha: sha, // the blob SHA
-        // branch: ''
-    }
-
-    // $branch = $('#branch').val().trim()
-    // if ($branch != '') {
-    //     config.branch = $branch
-    // }
-
-    repo.contents($filePath).add(config)
-        .then((info) => {
-            console.log('File Updated. new sha is ', info.commit.sha)
-            // sha = info.commit.sha
-            // showTitle("sha=" + sha)
-
-            toastr.info("推送成功")
-            // 重新拉取
-            pullContent()
-        })
-}
-
-
-
-
-
-
-
 
 
 var check = function () {
@@ -203,16 +129,14 @@ var init = function () {
     document.getElementById('updateToken').onclick = setToken;
     document.getElementById('updateOwner').onclick = setOwner;
     document.getElementById('updateRepo').onclick = setRepo;
-    document.getElementById('updateFilePath').onclick = setFilePath;
 
-    document.getElementById('pullBtn').onclick = pullContent;
-    document.getElementById('pushBtn').onclick = pushContent;
+    // document.getElementById('pullBtn').onclick = pullContent;
+    // document.getElementById('pushBtn').onclick = pushContent;
 
     // 初始化内容
     getToken();
     getOwner();
     getRepo();
-    getFilePath();
 
     // 前置检查
     check();
@@ -228,3 +152,7 @@ var init = function () {
 
 
 init()
+
+query()
+
+// create('API创建标题', '内容')
