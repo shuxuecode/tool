419fab29c0b2c762cd8d58b3740fc846a5309dfa
shuxuecode <zhaoshuxue@163.com>
2025-02-10 20:36:57 +0800

    :stethoscope: :coffin: :mute:

--- todolist/js/main.js
+++ todolist/js/main.js
@@ -113,7 +113,9 @@ var init = function () {
     document.getElementById('updateOwner').onclick = setOwner;
     document.getElementById('updateRepo').onclick = setRepo;
     
-    document.getElementById('addBtn').onclick = setRepo;
+    document.getElementById('addBtn').onclick = function() {
+        modal.style.display = "block";
+    }
 
     
 
@@ -143,3 +145,26 @@ init()
 
 
 // create('API创建标题', '内容')
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
